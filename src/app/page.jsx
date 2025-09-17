c57dd8dc952c54ab8de710dc201ba4166bf25eb9
Dayezzz <adiasmuhsin1206@gmail.com>
2025-09-17 13:52:36 +0700

    Fix Import

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -4,6 +4,7 @@ import Navbar from "@/components/layout/navbar/navbar";
 import About from "@/components/views/About/about";
 import Faq from "@/components/views/Faq/faq";
 import Beranda from "@/components/views/Home/home";
+import ProductHome from "@/components/views/ProductHome/ProductHome";
 import Testimonial from "@/components/views/Testimonials/testimonials";
 
 export default function Home() {
