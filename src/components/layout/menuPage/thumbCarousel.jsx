c21addee1f449c7530b72d3a53553d91cff0c453
Dayezzz <adiasmuhsin1206@gmail.com>
2025-10-21 20:42:55 +0700

    Database Products

--- src/components/layout/menuPage/thumbCarousel.jsx
+++ src/components/layout/menuPage/thumbCarousel.jsx
@@ -2,9 +2,12 @@ import React, { useEffect, useRef, useState } from "react";
 import useEmblaCarousel from "embla-carousel-react";
 import { motion, useInView } from "framer-motion";
 
-export default function ThumbCarousel({ images }) {
+export default function ThumbCarousel({ images, varian }) {
   const [selectedIndex, setSelectedIndex] = useState(0);
   const [emblaRef, emblaApi] = useEmblaCarousel({ loop: false });
+
+  const ref = useRef(null);
+  const isInView = useInView(ref, { once: false }); 
 
   useEffect(() => {
     if (!emblaApi) return;
@@ -13,42 +16,45 @@ export default function ThumbCarousel({ images }) {
     onSelect();
   }, [emblaApi]);
 
-  const ref = useRef(null);
-  const isInView = useInView(ref);
-
   return (
     <motion.div
       ref={ref}
       initial={{ opacity: 0, x: -50 }}
       animate={{ opacity: isInView ? 1 : 0, x: isInView ? 0 : -50 }}
       transition={{ duration: 0.7, ease: "easeInOut" }}
-      className="flex lg:flex-row flex-col items-center lg:w-2/3 w-full lg:gap-4 gap-2"
+      className="flex lg:flex-row flex-col items-center lg:w-2/3 w-full lg:h-[80vh] h-fit lg:gap-4 gap-2"
     >
-      <div className="lg:order-1 order-2 flex lg:flex-col flex-row justify-center lg:gap-4 gap-2 lg:w-1/4 w-full">
+      <div className="lg:order-1 order-2 flex lg:flex-col flex-row justify-center lg:gap-4 gap-2 lg:w-1/4 w-full h-full">
         {images.map((img, index) => (
           <button
             key={index}
             onClick={() => setSelectedIndex(index)}
-            className={`w-full lg:h-40 h-20 lg:rounded-3xl rounded-2xl overflow-hidden transition-all duration-200 ${
+            className={`w-full lg:h-full h-20 lg:rounded-3xl rounded-2xl overflow-hidden transition-all duration-200 ${
               selectedIndex === index
-                ? ""
-                : "border-transparent opacity-70 hover:opacity-100"
+                ? "ring-2 ring-yellow-400"
+                : "opacity-70 hover:opacity-100"
             }`}
           >
             <img
-              src={img}
+              src={img.image_url}
               alt={`Thumbnail ${index}`}
               className="w-full h-full object-cover"
             />
           </button>
         ))}
       </div>
-      <div className="lg:order-2 order-1 lg:w-3/4 w-full h-full overflow-hidden lg:rounded-3xl rounded-2xl">
+
+      <div className="lg:order-2 order-1 lg:w-3/4 w-full h-full overflow-hidden lg:rounded-3xl rounded-2xl relative">
         <img
-          src={images[selectedIndex]}
+          src={images[selectedIndex].image_url}
           alt={`Main Image ${selectedIndex}`}
           className="w-full h-full object-cover transition-all duration-300"
         />
+        {varian && (
+          <p className="absolute top-5 right-5 text-lg font-semibold font-instrument gradiasi-btn-merah text-yellow-300 w-fit py-1.5 px-6 rounded-full">
+            {varian}
+          </p>
+        )}
       </div>
     </motion.div>
   );
--- src/components/views/Menu/menu.jsx
+++ src/components/views/Menu/menu.jsx
@@ -1,82 +1,111 @@
 import ThumbCarousel from "@/components/layout/menuPage/thumbCarousel";
 import { Button } from "@/components/ui/button";
+import supabase from "@/lib/db";
 import { motion, useInView } from "framer-motion";
 import { ShoppingBag } from "lucide-react";
-import { useRef } from "react";
-
-const images = [
-  "/pedas_ori/1.jpeg",
-  "/pedas_ori/4.jpeg",
-  "/pedas_ori/6.jpeg",
-  "/pedas_ori/3.jpeg",
-];
+import { useEffect, useRef, useState } from "react";
 
 export default function Menu() {
-  const ref = useRef(null);
-  const isInView = useInView(ref);
-  return (
-    <>
-      <div className="w-full flex flex-col items-center lg:pt-28 pt-20 pb-8 lg:px-16 px-6 font-poppins">
-        <motion.div
-          ref={ref}
-          initial={{ opacity: 0, y: -50 }}
-          animate={{ opacity: isInView ? 1 : 0, y: isInView ? 0 : -50 }}
-          transition={{ duration: 0.7, ease: "easeInOut" }}
-          className="w-full flex flex-col lg:items-start items-center lg:gap-2 gap-1"
-        >
-          <h1 className="lg:text-6xl text-4xl gradiasi-btn-merah text-transparent bg-clip-text font-semibold py-2">
-            Menu Pentol Ngetop
-          </h1>
-          <p className="lg:text-xl text-lg lg:text-left text-center lg:px-0 px-2">
-            Rasakan sensasi pedas nagih dengan menu pentol ngetop
-          </p>
-        </motion.div>
-        <div className="w-full flex lg:flex-row flex-col mt-14 lg:pr-16 pr-0 lg:gap-8 gap-2">
-          <ThumbCarousel images={images} />
-          <motion.div
-            ref={ref}
-            initial={{ opacity: 0, x: 50 }}
-            animate={{ opacity: isInView ? 1 : 0, x: isInView ? 0 : 50 }}
-            transition={{ duration: 0.7, ease: "easeInOut" }}
-            className="flex flex-col lg:justify-between justify-start lg:w-1/3 w-full py-6"
-          >
-            <div className="flex flex-col gap-4">
-              <h1 className="lg:text-5xl text-3xl font-semibold">
-                Pentol Pedas Banget
-              </h1>
-              <p>
-                Varian rasa pentol pedas banget, Pentol pedas kami terbuat dari
-                daging Ayam Segar pilihan dengan tekstur kenyal dan rasa gurih
-                yang pas di lidah. Ditambah dengan bumbu rempah asli dan sambal
-                pedas khas, menciptakan rasa gurih dan pedas yang menggugah
-                selera. Setiap bulatan pentol dibalut dengan bumbu pedas khas
-                yang meresap sempurna, memberikan sensasi pedas nikmat yang
-                bikin nagih. Cocok disantap kapan saja, baik untuk camilan
-                santai, teman belajar, maupun pelengkap saat kumpul bersama
-                teman dan keluarga.
-              </p>
-              <p className="text-lg font-bold font-inter gradiasi-btn-merah text-yellow-300 w-fit py-1 px-4 rounded-full">Stok : 202</p>
-              <span className="font-semibold font-gabarito text-xl">
-                2k++ Terjual
-              </span>
-            </div>
-            <div className="flex flex-col gap-6 pb-6">
-              <p className="text-3xl mt-4 font-inter gradiasi-btn-merah text-transparent bg-clip-text font-bold">
-                Rp. 20.000
-              </p>
-              <Button
-                className={
-                  "gradiasi-btn-merah rounded-full text-yellow-300 w-full py-6 text-lg"
-                }
-                size={"sm"}
-              >
-                <ShoppingBag size={20} />
-                Pesan Sekarang
-              </Button>
-            </div>
-          </motion.div>
-        </div>
+  const [error, setError] = useState(null);
+  const [isLoading, setIsLoading] = useState(true);
+  const [product, setProduct] = useState([]);
+
+  const getProduct = async () => {
+    try {
+      const { data, error } = await supabase
+        .from("product")
+        .select(
+          "nama, deskripsi, varian, harga, stok, product_images (image_url)"
+        );
+      if (error) {
+        setError(error.message);
+      } else {
+        setProduct(data);
+      }
+      setIsLoading(false);
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
+  if (isLoading)
+    return (
+      <div className="w-full h-dvh py-20 flex justify-center items-center">
+        <p className="text-2xl font-semibold font-gabarito gradiasi-btn-merah text-transparent bg-clip-text">
+          Loading...
+        </p>
       </div>
-    </>
+    );
+
+  return (
+    <div className="w-full flex flex-col items-center lg:pt-28 pt-20 pb-8 lg:px-16 px-6 font-poppins overflow-hidden">
+      <motion.div
+        initial={{ opacity: 0, y: -50 }}
+        whileInView={{ opacity: 1, y: 0 }}
+        viewport={{ once: false, amount: 0.4 }}
+        transition={{ duration: 0.7, ease: "easeInOut" }}
+        className="w-full flex flex-col lg:items-start items-center lg:gap-2 gap-1"
+      >
+        <h1 className="lg:text-6xl text-4xl gradiasi-btn-merah text-transparent bg-clip-text font-semibold py-2">
+          Menu Pentol Ngetop
+        </h1>
+        <p className="lg:text-xl text-lg lg:text-left text-center lg:px-0 px-2">
+          Rasakan sensasi pedas nagih dengan menu pentol ngetop
+        </p>
+      </motion.div>
+
+      <div className="w-full flex flex-col items-center gap-4">
+        {product.map((item, index) => (
+          <ProductItem key={index} item={item} />
+        ))}
+      </div>
+    </div>
+  );
+}
+
+function ProductItem({ item }) {
+  const refRight = useRef(null);
+  const isInViewRight = useInView(refRight, { once: false });
+
+  return (
+    <div className="w-full flex lg:flex-row flex-col mt-14 lg:pr-16 pr-0 lg:gap-8 gap-8">
+      <ThumbCarousel images={item.product_images} varian={item.varian} />
+
+      <motion.div
+        ref={refRight}
+        initial={{ opacity: 0, x: 50 }}
+        animate={{
+          opacity: isInViewRight ? 1 : 0,
+          x: isInViewRight ? 0 : 50,
+        }}
+        transition={{ duration: 0.7, ease: "easeInOut" }}
+        className="flex flex-col lg:justify-between justify-start lg:w-1/3 w-full"
+      >
+        <div className="flex flex-col gap-4">
+          <h1 className="lg:text-5xl text-3xl font-semibold">{item.nama}</h1>
+          <p>{item.deskripsi}</p>
+          <p className="text-lg font-bold font-inter gradiasi-btn-merah text-yellow-300 w-fit py-1 px-4 rounded-full">
+            Stok : {item.stok}
+          </p>
+        </div>
+
+        <div className="flex flex-col gap-6 pb-6">
+          <p className="text-3xl mt-4 font-inter gradiasi-btn-merah text-transparent bg-clip-text font-bold">
+            Rp. {item.harga.toLocaleString("id-ID")}
+          </p>
+          <Button
+            className="gradiasi-btn-merah rounded-full text-yellow-300 w-full py-6 text-lg"
+            size="sm"
+          >
+            <ShoppingBag size={20} />
+            Pesan Sekarang
+          </Button>
+        </div>
+      </motion.div>
+    </div>
   );
 }
