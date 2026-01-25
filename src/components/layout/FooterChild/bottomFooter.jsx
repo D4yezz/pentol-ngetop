d4046f12314ed7795a6f36e095d64a12b602e0e4
Dayezzz <adiasmuhsin1206@gmail.com>
2026-01-25 10:44:30 +0700

    style(text): change year in footer using function Date

--- src/components/layout/FooterChild/bottomFooter.jsx
+++ src/components/layout/FooterChild/bottomFooter.jsx
@@ -1,9 +1,10 @@
 export default function BottomFooter() {
+  const year = new Date().getFullYear();
   return (
     <>
       <div className="flex justify-center lg:gap-0 gap-3 text-sm mt-12 w-full mx-auto h-fit border-t-[1.5px] border-yellow-300 font-inter py-4">
         <span className="lg:text-sm text-xs">
-          © 2025 Pentol Ngetop. Semua hak dilindungi undang-undang.
+          © {year} Pentol Ngetop. Semua hak dilindungi undang-undang.
         </span>
         {/* <div className="flex lg:gap-6 gap-2 lg:text-sm text-xs">
           <a href="/">Syarat & Ketentuan</a>
